a06365d5d3d392771a9298b14b91db6319fd9e61
Rahul Gupta <rahulgupta@ubuntu.(none)>
2012-02-19 15:33:59 -0800

    added a model boilerplate

--- public/javascripts/models/modelboilerplate.js
+++ public/javascripts/models/modelboilerplate.js
@@ -4,7 +4,7 @@ define([
 	'now'
 ], function($, _, Backbone){
 	var ??? = Backbone.Model.extend({
-	
+		//urlRoot: ONLY IF model is not part of a collection
 	});
   return (new) ???; //new only if the model is not part of a collection
 });
