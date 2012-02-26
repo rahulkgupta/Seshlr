32585663c14e8e2998b02fb375a808fa62136176
Rahul Gupta <rahulgupta@ubuntu.(none)>
2012-02-25 17:18:07 -0800

    trying stuff on heroku

---
+++ public/javascripts/views/home/seshview.js
@@ -0,0 +1,33 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+	'text!templates/seshfeed.html'
+], function($, _, Backbone, seshFeedTemplate){
+	var SeshFeedView = Backbone.View.extend({
+		el: $("#session-feed"),
+
+		events: {
+			'click .add-session' : 'addSession',
+
+		},
+		initialize: function (sesh, added) {
+			this.model = sesh;
+			this.added = added;
+		},
+		render: function () {
+				var data = {
+					_: _,
+					sesh: this.model,
+					added: this.added
+				};
+				var compiledTemplate = _.template( seshFeedTemplate, data );
+				$(this.el).append(compiledTemplate)
+		},
+
+		addSession: function (event) {
+			now.addSession(this.model.id);
+		},
+	});
+  return SeshFeedView;
+});
