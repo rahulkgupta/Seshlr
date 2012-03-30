efddd8fa52b544129991986c9938766597ecd7e7
Neal Chandra <chandra.neal@gmail.com>
2012-03-30 02:31:03 -0700

    Got calendar in place -- still need to set it up to work with filtering

--- public/javascripts/views/includes/calendar.js
+++ public/javascripts/views/includes/calendar.js
@@ -2,21 +2,18 @@ define([
   'jquery',
   'underscore',
   'backbone',
-], function($, _, Backbone){
+  'text!/../templates/calendar.html',
+], function($, _, Backbone, calendarTemplate){
 	var searchView = Backbone.View.extend({
-		el : '#filter-calendar',
-		
+		el: '#calendar-container',
+
 		events: {
+			'click .calendar-prev': 'scrollPrev',
+			'click .calendar-next': 'scrollNext',
 		},
 		
 		initialize: function(courses) {
 			// Just some quickly written functions we need. This shit is all pretty hacky.
-			is_equal = function(x1, x2) {
-				// This is so bad its not funny.
-				if (x1[0] == x2[0] && x1[1] == x2[1]) {
-					return true
-				} else { return false } 
-			}
 			modular_day = function(day, diff) {
 				day_index = day + diff
 				new_index = day_index % 7
@@ -24,6 +21,28 @@ define([
 					new_index += 7
 				}
 				return new_index
+			}	
+			// Defining my CalDate class.
+			function CalDate(month, date, day) {
+				this.month = month;
+				this.date = date;
+				this.day = day;
+				this.current = false;
+			}
+			CalDate.prototype.equals = function(other) {
+				if (this.month == other.month && this.date == other.date && this.day == other.day) {
+					return true
+				}
+				else {
+					return false
+				}
+			}
+			CalDate.prototype.convertNames = function() {
+				this.month = monthNames[this.month]
+				this.day = dayNames[this.day]
+			}
+			CalDate.prototype.get = function(attr) {
+				return this.attr;
 			}
 
 			var current = new Date();  
@@ -32,7 +51,6 @@ define([
 			var day = current.getDay();
 			var year = current.getFullYear();
 
-			console.log(day)
 			// Leap year handling
 			if (month == 1) {  
 				if ( (year%100!=0) && (year%4==0) || (year%400==0)){  
@@ -48,36 +66,36 @@ define([
 			var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 			var totalDays = ["31", ""+totalFeb+"","31","30","31","30","31","31","30","31","30","31"];
 
-			var today = [monthNames[month], date, day]
-			var date_array = []
+			var today = new CalDate(month, date, day)
+			var date_list = []
 
 			// We want to support (for now) browsing up to two weeks in either direction from the current date. To cover all cases lets find up to 20 days before and after today.
 			var start_day = modular_day(day, -20)
 			if (date - 20 > 0) {
-				var start_date = [month, date - 20, start_day]
-				var month_index = month
+				var start = new CalDate(month, date - 20, start_day)
 			}
 			else {
-				var month_index = month - 1
 				diff = 20 - date
-				var start_date = [month_index, totalDays[month_index] - diff, start_day]
+				var start = new CalDate(month - 1, totalDays[month_index] - diff, start_day)
 			}
 
 			// This is pretty filthy code but I think it works.
 			n=0;
-			month_val = 0;
-			iter_date = start_date[1];
-			for (i=0; i<=41; i++) {
-				iter_month = month_index + month_val
+			iter_date = start.date;
+			iter_month = start.month;
+			console.log(iter_date)
+			for (i=0; i<=88; i++) {
 				if (iter_date <= totalDays[iter_month]) {
-					temp_date = [monthNames[iter_month], iter_date, dayNames[modular_day(start_day, i)]]
-					if (is_equal(temp_date, today)) {
-						temp_date.push(true);
+					temp_date = new CalDate(iter_month, iter_date, modular_day(start_day, i))
+					if (temp_date.equals(today)) {
+						cursor = i;
+						temp_date.current = true;
 					}
-					date_array.push(temp_date)
+					temp_date.convertNames()
+					date_list.push(temp_date)
 				}
 				else {
-					month_val += 1
+					iter_month += 1
 					n = 0
 					iter_date = 0;
 					i -= 1;
@@ -85,14 +103,45 @@ define([
 				n++
 				iter_date = iter_date + 1
 			}
-
-			console.log(date_array)
-
+			
+			cursor = cursor - today.day;
+			this.days = date_list;
+			this.render();
 		},
 		
 		render: function () {
-			
+			var data = {
+				_: _,
+				days: this.days,
+			};
+			console.log(this.days)
+			var compiledTemplate = _.template( calendarTemplate, data );
+			$('#calendar').append(compiledTemplate);
+
+			this.cal_loc = -100 * cursor;
+			console.log(this.cal_loc)
+			$('#calendar ul').css('margin-left', this.cal_loc - 1)
 		},
+
+		scrollPrev: function(e) {
+			this.cal_loc += 700;
+			$('#calendar ul').animate({
+				marginLeft: this.cal_loc - 1,
+			});
+			e.preventDefault();
+		},
+
+		scrollNext: function(e) {
+			this.cal_loc -= 700;
+			$('#calendar ul').animate({
+				marginLeft: this.cal_loc - 1,
+			});
+			e.preventDefault();
+		},
+
+		test: function() {
+			alert('test');
+		}
 
 	});
   return searchView;
